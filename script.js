d94cc98813d611b569cce3d0982918c259e55cae
C137Rodrigolima <rodrigolimac137@gmail.com>
2021-11-28 01:13:31 -0300

    Encomenda a escolha ou via pedidos recentes funcionais

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
 let nome = prompt("Qual seu nome?");
-let model, neck, material, image, owner, author;
-let NumerodeSeleções =0;
+let model, neck, material, image, owner, author, IndiceEncomendaRecente, ObjetoEncomendaRecente;
+let NumerodeSeleções = 0;
 
-
-Promessa = axios.get('https://mock-api.driven.com.br/api/v4/shirts-api/shirts');
+function ChamarServidor() {
+let Promessa = axios.get('https://mock-api.driven.com.br/api/v4/shirts-api/shirts');
 Promessa.then(ColocarUltimosPedidos);
+}
+ChamarServidor();
 
 
 function ColocarUltimosPedidos(resposta) {
     let UltimosPedidos = resposta.data;
-    console.log(UltimosPedidos);
     const ConteinerdeUltimosPedidos = document.querySelector(".conteiner-de-pedidos");
 
     for(let i=0; i<UltimosPedidos.length; i++){
         ConteinerdeUltimosPedidos.innerHTML += `
-        <div class="bloco-pedido"><img class="img-pedido" src=${UltimosPedidos[i].image}><h4><span>Criador: </span>${UltimosPedidos[i].owner}</h4><div>
+        <div class="bloco-pedido" onclick="RequerirConfirmação(${i})"><img class="img-pedido" src=${UltimosPedidos[i].image}><h4><span>Criador: </span>${UltimosPedidos[i].owner}</h4><div>
         `;
+    }
+}
+
+function RequerirConfirmação(numerorecebido) {
+    let ConfirmacaoEncomenda = window.confirm("Confirma o pedido de uma encomenda via Últimos Pedidos?");
+    IndiceEncomendaRecente = numerorecebido;
+
+    if (ConfirmacaoEncomenda){        
+        let PromessaEncomendaRecente = axios.get('https://mock-api.driven.com.br/api/v4/shirts-api/shirts');
+        PromessaEncomendaRecente.then(EncomendarPedidoRecentes);
+    }
+}
+function EncomendarPedidoRecentes(Resposta){
+    const itens = Resposta.data;
+    ObjetoEncomendaRecente = {
+        model: itens[IndiceEncomendaRecente].model,
+        neck: itens[IndiceEncomendaRecente].neck,
+        material: itens[IndiceEncomendaRecente].material,
+        image: itens[IndiceEncomendaRecente].image,
+        owner: itens[IndiceEncomendaRecente].owner,
+        author: itens[IndiceEncomendaRecente].owner
     }
 }
 
 function EscolherModelo(blococlicadoagora) {
     const BlocoJaSelecionado = document.querySelector('.bloco-imagem1.escolhido');
-    
+
     if (BlocoJaSelecionado !== null){
         BlocoJaSelecionado.classList.remove('escolhido');
         NumerodeSeleções--;
@@ -83,7 +105,7 @@ function ValidarEncomenda() {
     let input = document.querySelector("input");
     let InputUrlImagem = input.value;
 
-    if(NumerodeSeleções===3 && ChecaImagem(InputUrlImagem)){
+    if((NumerodeSeleções===3 && ChecaImagem(InputUrlImagem)) || ObjetoEncomendaRecente !== undefined){
         botao.classList.add('clicavel');
         botao.addEventListener('click', ProcessarEncomenda);
         Imagem = InputUrlImagem;
@@ -100,26 +122,31 @@ function ChecaImagem(testarurlink) {
 function ProcessarEncomenda() {
     alert("Sua encomenda está sendo Processada.");
 
-    Objeto = {
-        model: modelo,
-        neck: pescoço,
-        material: tipoMaterial,
-        image: Imagem,
-        owner: nome,
-        author: nome
+    if(ObjetoEncomendaRecente !== undefined){
+        const PromessaEnvio = axios.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts', ObjetoEncomendaRecente);
+        PromessaEnvio.then(ConfirmarEncomenda);
+        PromessaEnvio.catch(TratarErro);
+    } else {
+        Objeto = {
+            model: modelo,
+            neck: pescoço,
+            material: tipoMaterial,
+            image: Imagem,
+            owner: nome,
+            author: nome
+        }
+        const PromessaEnvio = axios.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts', Objeto);
+        PromessaEnvio.then(ConfirmarEncomenda);
+        PromessaEnvio.catch(TratarErro);
     }
-    console.log(Objeto);
-
-    const PromessaEnvio = axios.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts', Objeto);
-    PromessaEnvio.then(ConfirmarEncomenda);
-    PromessaEnvio.catch(TratarErro);
 
     function TratarErro(Erro){
-        console.log(Erro.data);
         alert("Ops, não conseguimos processar sua encomenda");
     }
     function ConfirmarEncomenda(resposta) {
-        console.log(resposta.data);
-        alert("Obrigado! Sua encomenda foi Confirmada!!!")
+        alert("Obrigado! Sua encomenda foi Confirmada!!!");
+        const ConteinerdeUltimosPedidos = document.querySelector(".conteiner-de-pedidos");
+        ConteinerdeUltimosPedidos.innerHTML = "";
+        ChamarServidor();
     }
 }
